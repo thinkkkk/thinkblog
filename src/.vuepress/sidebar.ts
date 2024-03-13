7d4385c35cab875b8ea21875c7a9697ac9f080a7
nian <nianshu@yeahka.com>
2024-03-13 18:17:50 +0800

    init

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,26 +1,21 @@
-import { sidebar } from "vuepress-theme-hope";
+import {sidebar} from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    },
-  ],
+    "/": [
+        "",
+        {
+            text: "书籍",
+            icon: "book",
+            prefix: "book/",
+            link: "book/",
+            children: "structure",
+        },
+        {
+            text: "关于",
+            icon: "laptop-code",
+            prefix: "about/",
+            children: "structure",
+        },
+        "intro",
+    ],
 });
